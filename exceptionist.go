722e830604c8e10fdcd6a4c55782ed452ca9e23e
erhan.mutlu <erhan.mutlu@trendyol.com>
2020-04-26 17:41:57 +0300

    translation service

--- exceptionist.go
+++ exceptionist.go
@@ -20,49 +20,16 @@ func (err TranslatedError) Error() string{
 	return err.errorMessage
 }
 
-type translation struct {
-	errorCode string
-	errorMessage string
-}
-
-var defaultTranslation = translation{
-	errorCode: "0",
-	errorMessage: "Default.",
-}
-
-var t1 = translation{
-	errorCode: "1",
-	errorMessage: "Geçersiz değer.",
-}
-
-var t2 = translation{
-	errorCode: "2",
-	errorMessage: "Hata.",
-}
-
-var translations = map[string]translation{
-	"invalid.value": t1,
-	"error": t2,
-}
-
-func New(key string, args []interface{}) ObservedError {
+func NewObservedError(key string, args []interface{}) ObservedError {
 	return ObservedError{
 		Key: key,
 		Args: args,
 	}
 }
 
-func new(t translation) TranslatedError {
+func newTranslatedError(errorCode string, errorMessage string) TranslatedError {
 	return TranslatedError{
-		errorCode:    t.errorCode,
-		errorMessage: t.errorMessage,
+		errorCode:    errorCode,
+		errorMessage: errorMessage,
 	}
-}
-
-func (err ObservedError) Translate() TranslatedError{
-	if translation, ok := translations[err.Key]; ok {
-		return new(translation)
-	}
-
-	return new(defaultTranslation)
 }
---
+++ translator.go
@@ -0,0 +1,44 @@
+package exceptionist
+
+type translation struct {
+	errorCode string
+	errorMessage string
+}
+
+var defaultTranslation = translation{
+	errorCode: "0",
+	errorMessage: "Default.",
+}
+
+var t1 = translation{
+	errorCode:    "1",
+	errorMessage: "Geçersiz değer.",
+}
+
+var t2 = translation{
+	errorCode:    "2",
+	errorMessage: "Hata.",
+}
+
+type TranslationService struct {
+	translations *map[string]translation
+}
+
+func InitializeTranslationService() TranslationService {
+	var translations = map[string]translation{
+		"invalid.value": t1,
+		"error":         t2,
+	}
+
+	return TranslationService{translations: &translations}
+}
+
+func (translationService TranslationService) Translate(err ObservedError) TranslatedError {
+	translations := *translationService.translations
+
+	if translation, ok := translations[err.Key]; ok {
+		return newTranslatedError(translation.errorCode, translation.errorMessage)
+	}
+
+	return newTranslatedError(defaultTranslation.errorCode, defaultTranslation.errorMessage)
+}
