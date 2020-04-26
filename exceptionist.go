970cbe6bc86915259fa58b51ae5397dabea403ee
erhan.mutlu <erhan.mutlu@trendyol.com>
2020-04-26 13:55:34 +0300

    added error struct

--- exceptionist.go
+++ exceptionist.go
@@ -2,6 +2,22 @@ package exceptionist
 
 import "fmt"
 
+type ObservedError struct {
+	Key string
+	Args []interface{}
+}
+
+func (c ObservedError) Error() string{
+	return c.Key
+}
+
+func New(key string, args []interface{}) ObservedError {
+	return ObservedError{
+		Key: key,
+		Args: args,
+	}
+}
+
 func DoPrint(a string) {
 	fmt.Print("exceptionist" + a)
 }
