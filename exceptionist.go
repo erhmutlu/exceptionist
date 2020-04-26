bca4d49b2b69c89adfea8c3f710eebc233f5bc7f
erhan.mutlu <erhan.mutlu@trendyol.com>
2020-04-26 14:35:48 +0300

    added translated error struct

--- exceptionist.go
+++ exceptionist.go
@@ -2,13 +2,47 @@ package exceptionist
 
 import "fmt"
 
+type TranslatedError struct {
+	errorCode string
+	errorMessage string
+}
+
 type ObservedError struct {
 	Key string
 	Args []interface{}
 }
 
-func (c ObservedError) Error() string{
-	return c.Key
+func (err ObservedError) Error() string{
+	return fmt.Sprint(err.Key, "with args", err.Args)
+}
+
+func (err TranslatedError) Error() string{
+	return err.errorMessage
+}
+
+type translation struct {
+	errorCode string
+	errorMessage string
+}
+
+var defaultTranslation = translation{
+	errorCode: "0",
+	errorMessage: "Default.",
+}
+
+var t1 = translation{
+	errorCode: "1",
+	errorMessage: "Geçersiz değer.",
+}
+
+var t2 = translation{
+	errorCode: "2",
+	errorMessage: "Hata.",
+}
+
+var translations = map[string]translation{
+	"invalid.value": t1,
+	"error": t2,
 }
 
 func New(key string, args []interface{}) ObservedError {
@@ -18,6 +52,17 @@ func New(key string, args []interface{}) ObservedError {
 	}
 }
 
-func DoPrint(a string) {
-	fmt.Print("exceptionist" + a)
+func new(t translation) TranslatedError {
+	return TranslatedError{
+		errorCode:    t.errorCode,
+		errorMessage: t.errorMessage,
+	}
+}
+
+func (err ObservedError) Translate() TranslatedError{
+	if translation, ok := translations[err.Key]; ok {
+		return new(translation)
+	}
+
+	return new(defaultTranslation)
 }
